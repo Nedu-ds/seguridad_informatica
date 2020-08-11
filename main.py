b965ce0b7c501e06beb45ac69d5ffdbbc12f7a93
Nestor Duque <nestorduque.10@gmail.com>
2020-08-11 14:42:08 -0500

    Curso hasta Herencia de templates

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, request, make_response,redirect, render_template
 
 app = Flask(__name__)
 
-todos = ["TODO 1","TODO 2","TODO 3"]
+todos = ["Comprar Cafe","Solicitud de Compra","Vender Producto"]
 
 @app.route('/')
 def index():
